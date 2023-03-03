5812e0124353d7994455fb1e200e1dcaa2c8e6f3
Ken Umeda <kenumeda1@gmail.com>
2023-03-03 22:20:29 +1100

    create netflix homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React from "react";
 import "./App.css";
+import Row from "./Row";
+import requests from "./request";
+import Banner from "./Banner";
+import NavBar from "./NavBar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>hi</h1>
+    <div className="app">
+      <NavBar />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romantic Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumantaries} />
     </div>
   );
-}
+};
 
 export default App;
